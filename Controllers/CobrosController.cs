ed955fcdb34e7fb9b2612bd2bf9f9d2ea811d868
Euryomarbonilla23 <eury_bonilla@ucne.edu.do>
2021-03-10 19:04:50 -0400

    creando controllers vacios

---
+++ Controllers/CobrosController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcialAplicadaII.Controllers
+{
+    public class CobrosController
+    {
+    }
+}
